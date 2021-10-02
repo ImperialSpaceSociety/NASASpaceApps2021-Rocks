b1ab2dec40d1a6f51a000b05f90a9c54a56552f6
Michal Palič <michal.palic20@ic.ac.uk>
2021-10-02 20:46:06 +0100

    chore: End of day push

--- RotationViewer/js/scene.js
+++ RotationViewer/js/scene.js
@@ -43,7 +43,7 @@ function init() {
     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
     const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( document.getElementById('rtxCanvas').clientWidth,document.getElementById('rtxCanvas').clientWidth );
+    renderer.setSize( 0.9*document.getElementById('rtxCanvas').clientWidth,0.9*document.getElementById('rtxCanvas').clientWidth );
     document.getElementById('rtxCanvas').appendChild( renderer.domElement );
 
     // Temporary Light
