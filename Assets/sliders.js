67355820d5e2da703c87a549309b79a4de2a7f52
Michal Palič <michal.palic20@ic.ac.uk>
2021-10-03 11:45:54 +0100

    feat: event drive input

---
+++ Assets/sliders.js
@@ -0,0 +1,5 @@
+function slider_change(outID, inID){
+    document.getElementById(outID).innerHTML =
+        (parseFloat(document.getElementById(inID).value) / 1000 - 0.5).toPrecision(2) + " rad/s";
+
+}
--- RotationViewer/js/scene.js
+++ RotationViewer/js/scene.js
@@ -40,7 +40,7 @@ const MODELS = [
 function init() {
     // Creating the scene
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    const camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 1000 );
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize( 0.9*document.getElementById('rtxCanvas').clientWidth,0.9*document.getElementById('rtxCanvas').clientWidth );
